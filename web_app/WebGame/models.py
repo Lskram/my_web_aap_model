5c9172e2adf50c87e45b921ea070bc733a55fb80
เฉินหม่า <117138644+Lskram@users.noreply.github.com>
2023-07-27 12:11:06 +0700

    การสร้างพีฟริกซ์และตาราง

--- web_app/WebGame/models.py
+++ web_app/WebGame/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+prefix_choices =(
+    (1,"นาย"),
+    (2,"นางสาว"),
+    (3,"นาง"),
+)
+
 class Student(models.Model):
     std_id =    models.IntegerField()
+    prefix = models.IntegerField(choices=prefix_choices,    default=1)
     name=       models.CharField(max_length=255)
     lastname=   models.CharField(max_length=255)
     phone =     models.CharField(max_length=255)
@@ -9,11 +16,8 @@ class Student(models.Model):
     
 
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name = 'student'
+        verbose_name_plural = 'student'
 
     def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return self.name + " " + self.lastname
