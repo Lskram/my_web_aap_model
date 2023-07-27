c4d05d51032bd8d325af6cbeae5aeb0dd0602e3d
เฉินหม่า <117138644+Lskram@users.noreply.github.com>
2023-07-27 11:18:40 +0700

    add student model

--- web_app/WebGame/models.py
+++ web_app/WebGame/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    std_id =    models.IntegerField()
+    name=       models.CharField(max_length=255)
+    lastname=   models.CharField(max_length=255)
+    phone =     models.CharField(max_length=255)
+    address=    models.TextField()
+    
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
